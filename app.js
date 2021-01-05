73b6e349dde538ef185d098055ce1014538c361f
gusfahmi <gus@entertrim.com>
2021-01-05 18:14:45 +0700

    Updating app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require('cors'); 
 const axios = require('axios');
 const mime = require('mime-types');
 
